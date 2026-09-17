d12d648779c0092d1eb37c3db48e0438b899c5cb
Inflare12 <Inflare2907@gmail.com>
2026-09-17 16:35:40 +0530

    Export supervisor and integrity controls

--- airlock/__init__.py
+++ airlock/__init__.py
@@ -1,11 +1,14 @@
 from .audit import AuditLog
 from .containment import ContainmentController
 from .guard import Airlock, SecurityViolation
+from .integrity import sha256_file, sha256_json, verify_file, verify_policy_file
 from .kill_switch import KillSwitch
 from .monitor import RuntimeMonitor
 from .policy import Limits, Policy, redact_secrets
+from .supervisor import AISupervisor
 
 __all__ = [
-    "Airlock", "AuditLog", "ContainmentController", "KillSwitch",
+    "Airlock", "AISupervisor", "AuditLog", "ContainmentController", "KillSwitch",
     "Limits", "Policy", "RuntimeMonitor", "SecurityViolation", "redact_secrets",
+    "sha256_file", "sha256_json", "verify_file", "verify_policy_file",
 ]
